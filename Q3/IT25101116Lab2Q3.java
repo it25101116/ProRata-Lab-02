3411cbd59d92187eec09ad45f9508546792197db
it25101116 <it25101116@my.sliit.lk>
2026-09-22 19:55:39 +0530

    Add files via upload

---
+++ Q3/IT25101116Lab2Q3.java
@@ -0,0 +1,12 @@
+public class IT26101614Lab2Q3
+{
+        public static void main(String[] args){
+			
+			double sideA = 3;
+			double sideB = 4;
+			
+			double hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB));
+			
+			System.out.println("length of the hypotenuse:" + hypotenuse);
+		}
+}		
