399084f265338b9bdc94f7c43003659273cc1116
it25101116 <it25101116@my.sliit.lk>
2026-09-22 19:54:50 +0530

    Add files via upload

---
+++ Q1/IT25101116Lab2Q1.java
@@ -0,0 +1,17 @@
+public class IT26101614Lab2Q1{
+
+    public static void main(String[] args) {
+		
+	    double perimeter = 100;
+		
+		// width is 3/4 of length(3/4L)
+		// 100= 2*(L+ 3/4L)
+		
+		double length = perimeter/3.5;
+	    double width =	(3.0/4.0)* length;
+		
+		System.out.println("length of the fence" + length);
+		System.out.println("width of the fence" + width);
+	}
+}	
+		
